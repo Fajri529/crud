c8e0f3b0159c6831307ecbcca9d5ec2aa414b84c
Miftahul Fajri Sidqi <miftahulfajrisidqi529@gmail.com>
2021-10-20 15:18:51 +0700

    menambahkan foto

--- application/models/m_data.php
+++ application/models/m_data.php
@@ -1,27 +1,21 @@
 <?php
-class M_data extends CI_Model{
-
-    function input_data ($data,$table){
-        $this->db->insert ($table,$data);
+class m_data extends CI_Model {
     
-    }
+    function input_data ($data,$table) {
+        $this->db->insert($table,$data) ;
 
+    }
     function tampil_data () {
-        return $this->db->get('mahasiswa');
-    }
-    function edit_data ($where, $table){
-        return $this->db->get_where ($table,$where);
+        return $this->db->get ('mahasiswa');
     }
 
-    function update_data ($where, $data, $table){
-        $this->db->where ($where);
-        $this->db->update ($table, $data);
+    function edit_data ($where,$table) {
+        return $this->db->get_where ($table,$where) ;
     }
 
-    function hapus_data($where, $table){
-        $this->db->where($where);
-        $this->db->delete($table);
+    function update_data ($where,$data,$table) {
+        $this->db->$where($where) ;
+        $this->db->update($table,$data);
     }
 }
-
 ?>
